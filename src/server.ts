99787c7a61698c333363bf1c21fc2225f738b4fc
2305ray <rayssagarcia035@gmail.com>
2025-09-25 20:20:22 -0300

    pegar e enviar json

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 
-const PORT = 3333
+const PORT = 4013
 const app = express()
+
+app.use(express.json())
 
 app.get("/products/:id/:user", (req, res) => {
     const { page, limit } = req.query
@@ -9,4 +11,9 @@ app.get("/products/:id/:user", (req, res) => {
     res.send(`Página ${page} de ${limit}`)
 })
 
+app.post("/products", (req, res) => {
+    const {name, price} = req.body
+
+    res.status(201).json([name, price])
+})
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
