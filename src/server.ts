a78a9c5ba796988040e58ace09adf9e9f8bb46af
2305ray <rayssagarcia035@gmail.com>
2025-09-25 14:56:19 -0300

    recuperando parametros nomeados

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,10 @@ import express from 'express'
 const PORT = 3333
 const app = express()
 
+app.get("/products/:id/:user", (req, res) => {
+    const { id, user } = req.params
+
+    res.send(`Produto ${id} do candidato ${user}`)
+})
+
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
