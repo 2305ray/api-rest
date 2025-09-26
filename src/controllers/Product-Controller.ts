92ee99b0271c3e83de3b49042275e62bab2523bc
2305ray <rayssagarcia035@gmail.com>
2025-09-25 22:43:14 -0300

    import do zod e criação do schema

--- src/controllers/Product-Controller.ts
+++ src/controllers/Product-Controller.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import { AppError } from "../utils/App-Errors";
+import z from "zod"
 
 export class ProductsController {
     //ter no maximo 5 metodos
@@ -11,24 +12,13 @@ export class ProductsController {
     }
 
     create(req: Request, res: Response) {
-        const { name, price } = req.body
 
-        if (!name) {
-            throw new AppError("Nome do produto é obrigatório")
-        }
+        const bodySchema = z.object({
+            name: z.string().min(6, "Nome do produto tem que ter pelo menos 6 letras"),
+            price: z.number().min(0, "Preço do produto não pode ser negativo")
+        })
 
-        if (name.trim().length < 6) {
-            throw new AppError("Nome do produto tem que ter pelo menos 6 letras")
-        }
-
-        if (!price) {
-            throw new AppError("Preço do produto é obrigatório")
-        }
-
-        if (price < 0) {
-            throw new AppError("Preço do produto não pode ser negativo")
-        }
-
+        const { name, price } = bodySchema.parse(req.body)
         //throw new AppError("Erro ao criar produto")
 
         res.status(201).json({ name, price, user_id: req.user_id })
