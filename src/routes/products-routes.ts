77a0d9d73ae04c9b82a8028dacfad37f375b1185
2305ray <rayssagarcia035@gmail.com>
2025-09-25 21:04:20 -0300

    rotas

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,1 +1,17 @@
 import { Router } from "express";
+import { myMiddleware } from "../middlewares/my-middleware";
+
+export const productRoutes = Router()
+
+//local, em uma rota específica
+productRoutes.get("/", myMiddleware, (req, res) => {
+    const { page, limit } = req.query
+
+    res.send(`Página ${page} de ${limit}`)
+})
+
+productRoutes.post("/", (req, res) => {
+    const { name, price } = req.body
+
+    res.status(201).json([name, price])
+})
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
 import express from 'express'
-import { myMiddleware } from './middlewares/my-middleware'
+import { routes } from './routes'
 
 const PORT = 4013
 const app = express()
 
 app.use(express.json())
 
-// global: app.use(myMiddleware)
+//middleware global: app.use(myMiddleware)
+app.use(routes)
 
-//local, em uma rota específica
-app.get("/products/:id/:user", myMiddleware, (req, res) => {
-    const { page, limit } = req.query
-
-    res.send(`Página ${page} de ${limit}`)
-})
-
-app.post("/products", (req, res) => {
-    const { name, price } = req.body
-
-    res.status(201).json([name, price])
-})
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
