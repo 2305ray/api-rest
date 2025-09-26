bae9e47e1158377dbfe029939e7ed2dec2a81c25
2305ray <rayssagarcia035@gmail.com>
2025-09-25 22:29:14 -0300

    testando condiçòes de erros

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -13,6 +13,22 @@ export class ProductsController {
     create(req: Request, res: Response) {
         const { name, price } = req.body
 
+        if (!name) {
+            throw new AppError("Nome do produto é obrigatório")
+        }
+
+        if (name.trim().length <6) {
+            throw new AppError("Nome do produto tem que ter pelo menos 6 letras")
+        }
+
+        if (!price) {
+            throw new AppError("Preço do produto é obrigatório")
+        }
+
+        if (price < 0) {
+            throw new AppError("Preço do produto não pode ser negativo")
+        }
+        
         //throw new AppError("Erro ao criar produto")
 
         res.status(201).json({ name, price, user_id: req.user_id })
