23c528f1d3c72862b0b8ae1589e5adf606733b4c
2305ray <rayssagarcia035@gmail.com>
2025-09-25 22:51:18 -0300

    formatando mensagens de erro

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { routes } from './routes'
 import express, { Response, Request, NextFunction } from 'express'
 import { AppError } from './utils/App-Errors'
+import { ZodError } from 'zod'
 
 const PORT = 4013
 const app = express()
@@ -13,6 +14,11 @@ app.use(routes)
 app.use((err: any, req: Request, res: Response, _: NextFunction) => {
     if (err instanceof AppError) {
         return res.status(err.statusCode).json({ message: err.message })
+    }
+    if(err instanceof ZodError) {
+        res.status(400).json({message: "Validations Error", issues: err.format()
+
+        })
     }
     res.status(500).json({ message: err.message || "Internal server error" })
 })
